c85b4e908b5008b8ebc09a5a9ef1e48ea4267a7f
elenakosenchuk <kotikkss@gmail.com>
2021-12-11 16:07:29 +0300

    add classwork ajax

--- 35_js_class_homework/assets/js/main.js
+++ 35_js_class_homework/assets/js/main.js
@@ -3,7 +3,6 @@ class Circle{
         this.radius = radius;
         this.color = color;       
     }
-
     createCircle = function(){
         let div = document.createElement('div');
         div.style.borderRadius = this.radiusPx;
@@ -50,14 +49,79 @@ class Circle{
         }
         return dl_info;
     }
+
+
+    // set setRadius(new_radius){
+    //     this.radius = new_radius;
+    // }
+    // get circleInfo(){
+    //     return{
+    //         radius:this.radius,
+    //         diametr:this.radius*2,
+    //         square:Math.PI*Math.pow(this.radius, 2)
+    //     }
+    // }
+    
+    // function printCircleInfo(){
+    //     let cinfo = circleInfo;
+    //     for(let p in cinfo){
+    //         html += `<li></li>`;
+    //     }
+    //     rez_div.innerHTML = html;
+    // }
 } 
 
 const cir = new Circle(0);
 
 function createCircle(field_id, div_info, div_drow){    
-    document.getElementById(field_id).innerHTML = '';
     let r = document.getElementById(field_id).value;
     cir.radius = r;
-    document.getElementById(div_info).append(cir.html_info);
-    document.getElementById(div_drow).append(cir.createCircle());
+    document.getElementById(div_info).append(cir.html_info); // innerhtml
+    document.getElementById(div_drow).append(cir.createCircle());       
+}
+
+class Marker{
+    constructor(color='red',ink=40){
+        this.color = color;
+        this.ink = ink;
+        this.text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur in odit magni sed dicta dolorem iure at ea eligendi. Quidem quisquam assumenda sed voluptatem reprehenderit magnam. Quibusdam optio incidunt iste! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos architecto qui possimus quisquam, odit explicabo ut nulla, animi laudantium labore provident, repellat sequi illum sint et magnam expedita? Esse, nesciunt!';
+    }
+
+    createString = function(){
+        let str = this.text;        
+        let index = 0;
+        let ink_amount = this.ink;
+        while(ink_amount > 0 && index < str.length){
+            if(str[index] != " "){
+                ink_amount = ink_amount - 0.5;
+            }
+            index++;
+        }
+        let colorStr = str.substring(0,index);
+        let uncolorStr = str.substring((index+1), str.length);
+        colorStr = `<span style="background-color:red;">${colorStr}</span>`;
+        return colorStr + uncolorStr;
+    }
+    
+    printMarker = function(field_id){
+        // debugger;        
+        document.getElementById(field_id).innerHTML = this.createString();
+    }     
+}
+
+class reusableMarker extends Marker{
+    get ink(){
+        return this.i;
+    }
+    set ink(i){
+        this.i = parseInt(i) || 0;
+    }
+}
+
+const colorText = new Marker('red', 20);
+
+function printText(field_id, div_id){
+    let i = parseInt(document.getElementById(field_id).value);
+    colorText.ink += i;
+    colorText.printMarker(div_id);    
 }
---
+++ 36_js_ajacs/assets/js/main.js
@@ -0,0 +1,90 @@
+// let circle_wrap = document.createElement('div');
+// document.body.prepend(circle_wrap);
+
+// function drawCircle(r=10, bgc='tomato'){
+//     // let circle = document.createElement('div');
+//     // circle.style.width = (2*r)+'px';
+//     // circle.style.height = (2*r)+'px';
+//     // circle.style.backgroundColor = bgc;
+//     // circle.style.borderRadius = '50%';
+//     // circle_wrap.innerHTML = '';
+//     // circle_wrap.append(circle);
+
+//     circle_wrap.innerHTML = `<div style="width:${2*r}px;height:${2*r}px;background-color:${bgc};border-radius:50%"></div>`;    
+// }
+
+// drawCircle(50);
+// drawCircle(150);
+// drawCircle(200);
+
+/*
+C - create PUT
+R - read GET
+U - update POST
+D - delete DELETE
+*/
+
+function loadPage(page_url){
+    // const xhr = new XMLHttpRequest();
+    // xhr.open('GET', 'pages/main.html');
+    // xhr.send();    
+    // xhr.onreadystatechange = function(){
+    //     console.log(xhr);        
+    //     if(xhr.readyState === 4){
+    //         console.log(xhr.response);
+    //         document.getElementById("content").innerHTML = xhr.response;
+    //     }
+    // }
+
+    // fetch('pages/main.html')
+    //     .then((resp)=>{            
+    //         return resp.text();
+    //     })
+    //     .then((data)=>{            
+    //         document.getElementById("content").innerHTML = data;
+    //     });
+
+    // axios('pages/main.html')
+    // .then(resp=>{
+    //     console.log(resp);
+    //     document.getElementById("content").innerHTML = resp.data;
+    // });
+
+    // $.ajax({
+    //     url:'pages/main.html',
+    //     type:'get',
+    //     dataType: 'html',
+    //     success:function(html){
+    //         // let $html = $(html);
+    //         // let h1 = $html[0];
+    //         // console.log(h1);
+    //         $("#content").html(html);
+    //     }
+    // });
+
+    $.get(page_url, (html)=>{
+        $("#content").html(html);
+    });
+}
+
+// loadPage();
+
+$(function(){
+    loadPage('pages/main.html');
+    $.ajax({
+        url:'common/menu.json',
+        type:'get',
+        dataType:'json',
+        success:function(json){
+            let html = '';
+            for(let i=0;i<json.length;i++){
+                html += `<a class="me-3 py-2 text-dark text-decoration-none" href="pages/${json[i].file}.html">${json[i].name}</a>`;
+            }
+            $('#main_menu').html(html);         
+        } 
+    });
+    $(document).on('click', '#main_menu a', function(e){
+        e.preventDefault();
+        loadPage($(this).attr("href"));
+    });
+});
